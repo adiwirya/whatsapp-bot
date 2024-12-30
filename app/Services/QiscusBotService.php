45f9bf1c1397ff367c6a669580b565502554f72f
Adi Wirya <adi.wirya@student.umn.ac.id>
2024-12-30 17:32:14 +0700

    Show name

--- app/Services/QiscusBotService.php
+++ app/Services/QiscusBotService.php
@@ -11,26 +11,27 @@ class QiscusBotService
     protected
         $responses = [
             'default' => 'Saya adalah bot otomatis. Saya akan membantu Anda sebisa mungkin.',
-            'greeting' => 'Hai! Selamat datang di layanan JF3 kami. Ada yang bisa saya bantu?',
+            'greeting' => 'Selamat datang di layanan JF3 kami. Ada yang bisa saya bantu?',
         ];
 
     public function processMessage($payload)
     {
         $message = $payload['message']['text'];
+        $name = $payload['from']['name'];
 
-        return $this->handleBotResponse($message);
+        return $this->handleBotResponse($message, $name);
     }
 
-    protected function handleBotResponse($message)
+    protected function handleBotResponse($message, $name)
     {
 
 
-        $botResponse = $this->determineResponse($message, $this->responses);
+        $botResponse = $this->determineResponse($message, $this->responses, $name);
 
         return $botResponse;
     }
 
-    protected function determineResponse($message, $responses)
+    protected function determineResponse($message, $responses, $name)
     {
         $lowercaseMessage = strtolower($message);
 
@@ -42,8 +43,10 @@ class QiscusBotService
             $response['type'] = 'file_attachment';
             $response['payload'] = '{"url": "https://image-archive.developerhub.io/image/upload/22512/bn3dkt8x2grrv3rpzv9v/1576557437.png","caption": "Undagan untuk mengikuti webinar"}';
             return $response;
+        } else {
+            $response['message'] = 'Hai ' . $name . $responses['default'];
+            $response['type'] = 'text';
+            return $response;
         }
-
-        return $responses['default'];
     }
 }
